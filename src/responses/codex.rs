666123e02841b60ea1771cbd2bfb3a89e9c9d63d
HenriqueFLTorres <ricalisboa@gmail.com>
2024-04-08 20:24:34 -0300

    feat - add in_progress and completed sum to codex

--- src/responses/codex.rs
+++ src/responses/codex.rs
@@ -7,10 +7,10 @@ use serde::{ Deserialize, Deserializer, Serialize, Serializer };
 #[serde(rename_all(serialize = "snake_case", deserialize = "snake_case"))]
 pub struct CodexResponse {
     pub data: HashMap<String, Codex>,
-    #[serde(deserialize_with = "string_to_u32", serialize_with = "u32_to_string")]
-    #[serde(skip_deserializing)]
     #[serde(default)]
-    pub completed_codex: u32
+    pub in_progress: u32,
+    #[serde(default)]
+    pub completed: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -28,18 +28,12 @@ pub struct Codex {
     pub in_progress: u32,
 }
 
-pub fn string_to_u32<'de, D>(deserializer: D) -> Result<u32, D::Error>
-where
-    D: Deserializer<'de>,
-{
+pub fn string_to_u32<'de, D>(deserializer: D) -> Result<u32, D::Error> where D: Deserializer<'de> {
     let s: String = Deserialize::deserialize(deserializer)?;
     s.parse::<u32>().map_err(serde::de::Error::custom)
 }
 
-pub fn u32_to_string<S>(value: &u32, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
+pub fn u32_to_string<S>(value: &u32, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
     serializer.serialize_str(&value.to_string())
 }
 
@@ -55,18 +49,15 @@ pub async fn get_nft_codex(
     let response = client.get(request_url).send().await?.text().await?;
     let mut response_json: CodexResponse = serde_json::from_str(&response)?;
 
-    let mut x: Vec<u32> = Vec::new();
-    let c = response_json.data.iter().for_each(|i| {
-        x.push(i.1.completed);
+    let mut in_progress_total = 0;
+    let mut completed_total = 0;
+    response_json.data.iter().for_each(|(key, codex)| {
+        in_progress_total += codex.in_progress;
+        completed_total += codex.completed;
     });
 
-    let mut sum: u32 = 0;
-    x.into_iter().for_each(|num| {
-        sum += num
-    });
-
-    println!("{sum:#?}");
-    response_json.completed_codex = sum;
+    response_json.in_progress = in_progress_total;
+    response_json.completed = completed_total;
 
     Ok(response_json)
 }
