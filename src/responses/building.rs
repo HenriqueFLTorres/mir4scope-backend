5642c787ecdf1cfd0db378329ee5d0405c1ea94a
HenriqueFLTorres <ricalisboa@gmail.com>
2024-05-05 11:37:22 -0300

    feat - transform buildings value to int

--- src/responses/building.rs
+++ src/responses/building.rs
@@ -6,7 +6,6 @@ use crate::utils::get_response;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BuildingResponse {
-    pub code: u16,
     pub data: HashMap<String, BuildingObject>,
 }
 
@@ -21,18 +20,20 @@ pub struct BuildingObject {
 pub async fn get_nft_buildings(
     transport_id: i32,
     client: ClientWithMiddleware
-) -> anyhow::Result<HashMap<String, String>> {
+) -> anyhow::Result<HashMap<String, i32>> {
     let request_url = format!(
         "https://webapi.mir4global.com/nft/character/building?transportID={transport_id}&languageCode=en",
         transport_id = transport_id
     );
 
     let response_json: BuildingResponse = get_response(&client, request_url).await?;
-    
-    let building_hashmap: HashMap<String, String> = response_json.data
+
+    let building_hashmap: HashMap<String, i32> = response_json.data
         .iter()
         .map(|building_object| {
-            (building_object.1.building_name.clone(), building_object.1.building_level.clone())
+            let value_as_number = building_object.1.building_level.parse::<i32>().unwrap();
+
+            (building_object.1.building_name.clone(), value_as_number.clone())
         })
         .collect();
 
