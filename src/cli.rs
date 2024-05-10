e9c91a6d09567f645cf15efc4dff23218c348c8d
Guilherme Menezes <guilhermedeoliveira.menezes@gmail.com>
2024-05-10 16:26:24 -0300

    Fix: set default drop to false

--- src/cli.rs
+++ src/cli.rs
@@ -9,10 +9,10 @@ pub struct Cli {
     pub initial_page: u8,
 
     /// Final page to collect NFT
-    #[arg(short, long, default_value_t = 0)]
+    #[arg(short, long, default_value_t = 5)]
     pub final_page: u8,
 
-    /// If the backend should drop the database or not.
-    #[arg(short, long, default_value_t = true)]
+    /// If the backend should drop the database or not. [Default: false]
+    #[arg(short, long, default_value_t = false)]
     pub drop: bool,
 }
